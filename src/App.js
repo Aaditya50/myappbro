3819b9cc009d966d1cbc13ed4fb103084d83626c
Aaditya55 <aadityajoshi291020@gmail.com>
2023-01-10 14:26:04 +0530

    do

--- src/App.js
+++ src/App.js
@@ -6,22 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
-<<<<<<< HEAD
         <a>Chan ahe na tula avadal ka ?</a>
 
 
         <a>
           aapan ashi pn gappa maru shakto
-=======
-        <a>Hello Vaishu</a>
 
-        <a>
-          aushadh neet ghet ja ... ghasa chi kalgi ghe
->>>>>>> e0273db4fd1f345a6f3aaa8225750a698fc49acd
         </a>
       </header>
     </div>
